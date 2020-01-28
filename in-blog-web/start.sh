b7ef6db6fd53eadaca0d1b763533acfab15e6c7e
cuiyongxu <cuiyongxu@gmail.com>
2020-01-28 22:31:53 +0800

    update shell

--- in-blog-web/start.sh
+++ in-blog-web/start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-bash_path=/home/liyaping/in-blog-boot/
+bash_path=/Users/cuiyongxu/workspace/ijson/in-blog-boot
 
 cd $bash_path
 
-git pull
+#git pull
 
 mvn install -Dmaven.test.skip=true
 
@@ -22,11 +22,25 @@ pid=$(ps -ef | grep "in-blog-web" | grep -v grep | awk '{print $2}')
 
 echo "current in-blog-web pid:" $pid
 
-kill -9 $pid
+if [ ! -n "$pid" ];then
+    echo "service not started"
+else
+    kill -9 $pid
+fi
+
 
 DATE=$(date +%Y%m%d%H%M%S)
 cd $bash_path/in-blog-web
-mv $bash_path/in-blog-web/nohup.out $bash_path-log/$DATE.log
+
+if [ ! -f "$bash_path/in-blog-web/nohup.out" ]; then
+    if [ ! -d "$bash_path-log/" ]; then
+        mkdir $bash_path-log/
+    fi
+    mv $bash_path/in-blog-web/nohup.out $bash_path-log/$DATE.log
+fi
 
 cd $bash_path/in-blog-web/
 nohup mvn spring-boot:run >> nohup.out 2>&1 &
+
+pid=$(ps -ef | grep "in-blog-web" | grep -v grep | awk '{print $2}')
+echo "start in-blog-web success, pid:" $pid
