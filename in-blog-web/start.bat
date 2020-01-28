91bd4e1a9476bd68cd47552c3cae96fef8e37cd7
cuiyongxu <cuiyongxu@gmail.com>
2020-01-29 00:32:05 +0800

    add bat

--- in-blog-web/start.bat
+++ in-blog-web/start.bat
@@ -4,11 +4,17 @@ rem ---------------------------------------------------------------------------
 rem start in-blog-boot server
 rem ---------------------------------------------------------------------------
 
-set "IBO_HOME=%cd%"
+set WEB_HOME=%cd%
+
+set IBO_HOME=%WEB_HOME%/../
+
+echo "IBO_HOME:" %IBO_HOME%
 
 cd %IBO_HOME%
 
-git pull
+rem git pull
+
+mvn package -Dmaven.test.skip=true
 
 mvn install -Dmaven.test.skip=true
 
@@ -22,3 +28,18 @@ cd %IBO_HOME%/in-blog-core
 mvn clean install -Dmaven.test.skip=true
 
 cd %IBO_HOME%/in-blog-web
+
+
+for /f "tokens=5" %%i in ('netstat -aon ^| findstr ":8876"') do (
+    set pid=%%i
+)
+
+taskkill /f /pid %n%
+
+start /max mvn spring-boot:run
+
+for /f "tokens=5" %%i in ('netstat -aon ^| findstr ":8876"') do (
+    set pid=%%i
+)
+
+echo "start in-blog-web success,pid:" $pid
