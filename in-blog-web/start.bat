02a43167ce318c0829d3009c7f5d51e1743fb5f0
cuiyong_xu <cuiyongxu@gmail.com>
2020-05-29 17:50:58 +0800

    update windows shell start exception

--- in-blog-web/start.bat
+++ in-blog-web/start.bat
@@ -1,45 +1,53 @@
-@echo off
-
-rem ---------------------------------------------------------------------------
-rem start in-blog-boot server
-rem ---------------------------------------------------------------------------
+::common
+::remote
+::auth
+::core
+::web
+::start
 
 set WEB_HOME=%cd%
-
 set IBO_HOME=%WEB_HOME%/../
 
-echo "IBO_HOME:" %IBO_HOME%
 
+@echo off
+
+
+echo "IBO_HOME:" %IBO_HOME%
 cd %IBO_HOME%
 
-rem git pull
 
-mvn package -Dmaven.test.skip=true
+rem @echo build package
+rem call mvn package -Dmaven.test.skip=true
 
-mvn install -Dmaven.test.skip=true
 
+rem @echo build install
+rem call mvn install -Dmaven.test.skip=true
+
+
+@echo build common
 cd %IBO_HOME%/in-blog-common
-mvn clean install -Dmaven.test.skip=true
+call mvn install -Dmaven.test.skip=true
 
+
+@echo build remote
+cd %IBO_HOME%/in-blog-remote
+call mvn install -Dmaven.test.skip=true
+
+
+@echo build auth
 cd %IBO_HOME%/in-blog-auth
-mvn clean install -Dmaven.test.skip=true
+call mvn install -Dmaven.test.skip=true
 
+
+
+@echo build core
 cd %IBO_HOME%/in-blog-core
-mvn clean install -Dmaven.test.skip=true
+call mvn install -Dmaven.test.skip=true
 
+
+@echo build web
 cd %IBO_HOME%/in-blog-web
+call mvn install -Dmaven.test.skip=true
 
-
-for /f "tokens=5" %%i in ('netstat -aon ^| findstr ":8876"') do (
-    set pid=%%i
-)
-
-taskkill /f /pid %n%
-
-start /max mvn spring-boot:run
-
-for /f "tokens=5" %%i in ('netstat -aon ^| findstr ":8876"') do (
-    set pid=%%i
-)
-
-echo "start in-blog-web success,pid:" $pid
+@echo build start
+call mvn spring-boot:run >> run.log
