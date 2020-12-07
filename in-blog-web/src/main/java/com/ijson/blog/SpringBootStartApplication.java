750b48ed9f7e2ee97705fef8358506af10377556
cuiyongxu <cuiyongxu@gmail.com>
2020-12-08 01:27:27 +0800

    add tomcat

---
+++ in-blog-web/src/main/java/com/ijson/blog/SpringBootStartApplication.java
@@ -0,0 +1,13 @@
+package com.ijson.blog;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class SpringBootStartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(BlogWebBootstrap.class);
+    }
+}
---
+++ in-blog-web/tomcat.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+IBO_HOME=/Users/cuiyongxu/workspace/ijson/in-blog-boot
+TOMCAT_HOME=/Users/cuiyongxu/software/tomcat-8.5.58
+cd $IBO_HOME || exit
+
+#git pull
+
+mvn clean package install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dgpg.skip
+
+cd $IBO_HOME/in-blog-common || exit
+mvn clean install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dgpg.skip
+
+cd $IBO_HOME/in-blog-remote || exit
+mvn clean install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dgpg.skip
+
+cd $IBO_HOME/in-blog-auth || exit
+mvn clean install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dgpg.skip
+
+cd $IBO_HOME/in-blog-core || exit
+mvn clean install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dgpg.skip
+
+cd $IBO_HOME/in-blog-web || exit
+mvn clean package -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dgpg.skip=true
+
+pid=$(ps -ef | grep "in-blog-web" | grep -v grep | awk '{print $2}')
+
+echo "current in-blog-web pid:" $pid
+
+if [ ! -n "$pid" ];then
+    echo "service not started"
+else
+    kill -9 $pid
+fi
+
+rm -fr $TOMCAT_HOME/webapps/ROOT.war
+rm -fr $TOMCAT_HOME/webapps/ROOT
+
+cp -fr $IBO_HOME/in-blog-web/target/in-blog-web-1.0.1-SNAPSHOT.war $TOMCAT_HOME/webapps/ROOT.war
+
+cd $TOMCAT_HOME/bin || exit
+
+#sh shutdown.sh
+
+#sh startup.sh
+
+#echo "start tomcat"
+
+echo "done..."
