b431f455d102540170f6a70b90b2bf1109da6118
cuiyongxu <cuiyongxu@gmail.com>
2021-03-08 01:18:05 +0800

    splite application properties

--- in-blog-web/src/main/java/com/ijson/blog/BlogWebBootstrap.java
+++ in-blog-web/src/main/java/com/ijson/blog/BlogWebBootstrap.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @ImportResource(locations = {"classpath:applicationContext.xml"})
 @SpringBootApplication
 @EnableCaching
+@PropertySource(value = {"classpath:application.properties","classpath:administrator.properties"})
 public class BlogWebBootstrap {
 
     public static void main(String[] args) {
-        //System.setProperty("logback.configurationFile", "/Users/cuiyongxu/logback.xml");
         System.setProperty("file.encoding", "UTF-8");
         SpringApplication.run(BlogWebBootstrap.class, args);
     }
