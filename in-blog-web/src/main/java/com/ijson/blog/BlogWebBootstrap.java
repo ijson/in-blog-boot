db42373cd1576a0f9f76613713cf922d3c73253b
cuiyongxu <cuiyongxu@gmail.com>
2021-04-05 11:52:14 +0800

    upgrad rest-proxy

--- in-blog-web/src/main/java/com/ijson/blog/BlogWebBootstrap.java
+++ in-blog-web/src/main/java/com/ijson/blog/BlogWebBootstrap.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @ImportResource(locations = {"classpath:applicationContext.xml"})
 @SpringBootApplication
 @EnableCaching
+@PropertySource(value = {"classpath:application.properties","classpath:administrator.properties"})
 public class BlogWebBootstrap {
 
     public static void main(String[] args) {
